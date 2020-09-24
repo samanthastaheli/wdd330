90afee2ba5d54b6c50409e71a178e77fb1996970
samanthastaheli <sta180011@byui.edu>
2020-09-23 20:25:50 -0600

    practice

---
+++ practice/function-practice/function-practice.js
@@ -0,0 +1,27 @@
+//fucntion declaration
+function negative(num) {
+    return num * -1;
+}
+negative(3); //use
+
+//function expression
+const negative2(num) {
+    return num * -1;
+}
+negative2(3); //use
+
+//arrow function
+const negative3 = (num) => num * -1;
+negative3(3); //use
+
+//Simulate random dice roll
+//Get random number between 1-6
+console.log(Math.floor(Math.random()*6)+1);
+
+//Get random number between 22-36
+function rand(range, offset) {
+    return Math.floor(Math.random() * range) + offset;
+}
+//range = 14 offset = 22
+
+//random sentence generator 
---
+++ practice/loop-practice/loop-practice.js
@@ -0,0 +1,10 @@
+const num = [0, 1, 2, 3, 4, 5];
+
+for (let i = 0; i < num.length; i++) {
+    console.log(num[i]);
+    
+    // let output = num[i];
+    // document.getElementById('para').innerHTML = output;
+
+    // document.getElementById('para').innerHTML = num[i];
+}
