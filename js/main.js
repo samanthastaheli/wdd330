2814c822c9c7bdae1e24cd8fdf2a50e3c65ad6e6
samanthastaheli <sta180011@byui.edu>
2020-09-29 17:09:18 -0600

    link page changes

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const links = [
+    {  
+        label: 'Week1 Notes',
+        url: 'week1/index.html'
+    },
+    {
+        label: 'Week2 Notes',
+        url: 'week2/week2.html'
+    },
+    {
+        label: 'Week3 Notes',
+        url: 'week3/week3.html'
+    }
+];
+
+function renderlinkList(list) {
+    const listElement = document.querySelector('#linkList');
+    list.forEach((linkList) => {
+        const newli = document.createElement('li');
+        newli.innerHTML = renderOneLink(linkList);
+        listElement.appendChild(newli);
+    });
+}
+
+//return HTML string
+function renderOneLink(linkList) {
+    let linkHTML = `<li><a href='${linkList.url}'>${linkList.label}<a/></li>`
+}
+
+
+// for(let i = 0; i < links.length; i++) {
+//     // weekLabel = links[i].label;
+//     // weekLink = links[i].url;
+
+//     let li = document.createElement('li');
+//     let a = document.createElement('a');
+                   
+//     li.textContent = links[i].label;
+//     a.textContent = href='links[i].url';
+                
+//     li.appendChild(a);
+                
+//     document.getElementById('link').appendChild(li);
+
+//     // for (let x = 0; x <link.length; x++) {
+//     //     document.getElementById('link').innerHTML = w;
+//     // }
+// }
+
--- practice/hiking/hiking.js
+++ practice/hiking/hiking.js
@@ -42,9 +42,44 @@ const hikeList = [
 */
 
 function renderHikeList(list) {
-
+    const listElement = document.querySelector('#hikes'); //grabed element we will be working with
+    list.forEach((hike) => {    //forEach is looping through array
+        const newli = document.createElement('li'); //created li element 
+        newli.innerHTML = renderOneHike(hike);
+        listElement.appendChild(newli);
+    });
 }
 
+// returns HTML string
 function renderOneHike(hike) {
-     
+     let hikeHTML = `<img
+     src="https://byui-cit.github.io/cit261/examples/${hike.imgSrc}"
+     alt="${hike.imgAlt}"
+   />
+    <h2>${hike.name}</h2>
+   <div>
+     <h3>Distance</h3>
+     <p>3 miles</p>
+   </div>
+   <div>
+     <h3>Difficulty</h3>
+     <p>Easy</p>
+   </div>
+   <div>
+     <h3>Description</h3>
+     <p>Beautiful short hike along the Bechler river to Bechler Falls</p>
+   </div>
+   <div>
+     <h3>How to get there</h3>
+     <p>
+       Take Highway 20 north to Ashton. Turn right into the town and
+       continue through. Follow that road for a few miles then turn left
+       again onto the Cave Falls road. Drive to the end of the Cave Falls
+       road. There is a parking area at the trailhead.
+     </p>
+   </div>`
+   return hikeHTML;
 }
+
+// window.onload another way to run JS
+renderHikeList(hikeList);
