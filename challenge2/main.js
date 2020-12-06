93025b99e73f23b83e54a42ec5ce5d1b18bfd869
samanthastaheli <sta180011@byui.edu>
2020-12-05 18:49:22 -0800

    chlg 2 updates

--- challenge2/main.js
+++ challenge2/main.js
@@ -1,5 +1,9 @@
 import { buildQuiz } from './quiz.js';
 import { getJSON } from './quiz.js';
+import { getAnswers } from './quiz.js';
+// import { showResults } from './quiz.js';
 
 buildQuiz();
 console.log(getJSON("https://swapi.dev/api/people/"));
+
+document.getElementById('submit').addEventListener('click', getAnswers());
--- challenge2/quiz.js
+++ challenge2/quiz.js
@@ -1,10 +1,10 @@
-// variables 
+// Variables 
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit'); 
 const baseUrl = "https://swapi.dev/api/";
-const peopleUrl = "https://swapi.dev/api/people/";
 
+// Helper Functions
 // get JSON with helper function
 export function getJSON(url) {
     return fetch(url).then(function (response) { // what gets passed through respose ???
@@ -21,6 +21,7 @@ export function getJSON(url) {
     });
 }
 
+// Functions
 // create the questions
 export function buildQuiz() {
     const output = [] // var to store HTML output
@@ -48,6 +49,46 @@ export function buildQuiz() {
      quizContainer.innerHTML = output.join('');  
 }
 
+// get answers
+// activated by sumbmit btn event listener in main.js
+export function getAnswers() {
+  const score = [];
+  
+}
+
+// show results
+function showResults(){
+  // get answers from the quiz
+  const answerContainers = quizContainer.querySelectorAll('.answers');
+  // keep track of users answers
+  let numCorrect = 0;
+  // for each question...
+  myQuestions.forEach( (currentQuestion, questionNumber) => {
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    
+    // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
+
+      // color the answers green
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else{
+      // color the answers red
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+// questions & results arrays 
 const myQuestions = [
   {
     question: "Whose side are you on?",
@@ -75,5 +116,119 @@ const myQuestions = [
       d: "I don't use weapons",
       e: "What's a light saber?"
     },
+  },
+  {
+    question: "Who is your favorite character?",
+    options: {
+      a: "Han Solo",
+      b: "Mandalorian Di Dijan", // !!!! figure out how to spell his name 
+      c: "Yoda",
+      d: "Baby Yoda",
+      e: "Luke Skywalker",
+      f: "Darth Vadar",
+      g: "Obi One Kinoby", // !!!!! wrong spelling
+      h: "Princess Leia",
+      i: "Asoka Tono",
+      j: "Chewbacca",
+      k: "Ben Solo",
+      l: "Rei",
+      m: "Sand People"
+    },
+  },
+  {
+    question: "Who is your favorite pet or side kick?",
+    options: {
+      a: "The child",
+      b: "Chewbacca",
+      c: "R2-D2",
+      d: "C3PO",
+      e: "Porg",
+      f: "Ewok",
+      g: "Crazy snow spider",
+      h: "Banthas (sad peoples bison thing)",
+      i: "BB8", // !!!! check spelling
+      j: "Jawa"
+    },
+  },
+  {
+    question: "What is most important?",
+    options: {
+      a: "family",
+      b: "power", 
+      c: "surviving",
+      d: "killing all Jedis",
+      e: "defeating the empire",
+      f: "love",
+      g: "being able to bring people back from the dead"
+    },
+  }
+];
+
+const myResults = [
+  {
+    person: "https://swapi.dev/api/people/1/", // luke
+    ship: "https://swapi.dev/api/starships/12/",
+    weapon: "",
+    planet: "https://swapi.dev/api/planets/1/"
+  },
+  {
+    person: "", // han
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // leia
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // rei
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // vadar
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // sith
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // boba fet
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // mando
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // baby yoda
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // c3po
+    ship: "",
+    weapon: "",
+    planet: ""
+  },
+  {
+    person: "", // asoka
+    ship: "",
+    weapon: "",
+    planet: ""
   }
 ];
